8a6ce32305b212ecb4934ecc7ff84b83692cb6be
furukawa seiya <siroinu530@gmail.com>
2025-03-30 20:46:04 +0900

    リモートリポジトリの変更

--- src/database/migrations/2025_03_09_213004_create_profiles_table.php
+++ src/database/migrations/2025_03_09_213004_create_profiles_table.php
@@ -9,18 +9,18 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->string('image')->nullable();
-            $table->string('name');
-            $table->string('postal_code', 10);
-            $table->string('address');
-            $table->string('building')->nullable();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('profiles', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        $table->string('image')->nullable();
+        $table->string('name');
+        $table->string('postal_code', 10)->nullable();
+        $table->string('address')->nullable();
+        $table->string('building')->nullable();
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
